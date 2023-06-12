5e4f4c9b5c41bf435ee90f7ff605e0d3ab41956f
Marko S <marcodeee@gmail.com>
2023-06-12 18:04:01 +0200

    Create connection with DB

---
+++ HospitalInformationSystem/RepositoryProject/Context/HISConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryProject.Context
+{
+    public class HisConfiguration
+    {
+        public string HisConnection { get; set; }
+    }
+}
---
+++ HospitalInformationSystem/RepositoryProject/Context/HISContext.cs
@@ -0,0 +1,52 @@
+﻿using EntityProject;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryProject.Context
+{
+    public class HISContext : IdentityDbContext<ApiUser, IdentityRole, string>
+    {
+
+        public HISContext(DbContextOptions<HISContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Technician> Technicians { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Patient>().ToTable("Patient");
+            modelBuilder.Entity<Doctor>().ToTable("Doctor");
+            modelBuilder.Entity<Technician>().ToTable("Technician");
+            modelBuilder.Entity<Appointment>().ToTable("Appointment");
+
+
+            modelBuilder.Entity<Appointment>()
+           .HasOne<Patient>(s => s.Patient)
+           .WithMany(g => g.Appointments)
+           .HasForeignKey(s => s.PatientId);
+
+
+            modelBuilder.Entity<Appointment>()
+           .HasOne<Doctor>(s => s.Doctor)
+           .WithMany(g => g.Appointments)
+           .HasForeignKey(s => s.DoctorId);
+
+
+            modelBuilder
+            .Entity<Patient>()
+            .Property(e => e.Id)
+            .ValueGeneratedOnAdd();
+
+        }
+    }
+}
